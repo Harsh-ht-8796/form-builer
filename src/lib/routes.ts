fcd78001b77e14d5337e0fb779f968cfcb27bfeb
Harsh-8796 <harshvardhant@heaptrace.com>
2025-08-18 20:00:26 +0530

    update: route for public route

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -1,3 +1,3 @@
 export const ROOT = '/auth/login';
-export const PUBLIC_ROUTES = ['/auth/login'];
+export const PUBLIC_ROUTES = ['/auth/login',"/auth/signup","/auth/update-password"];
 export const DEFAULT_REDIRECT = '/';
